012b625383dda6be0420b821e175f7e596a832d4
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-26 17:28:56 +0500

    ch

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -67,7 +67,7 @@
                                                         class="list-group-item d-flex justify-content-between align-items-center border-top">
                                                         <div class="d-flex justify-content-center align-items-center">
                                                             <span class="mr-2 font-weight-bold font-italic">01</span>
-                                                            <img src="https://helostatus.com/wp-content/uploads/2021/09/HD-WhatsApp-profile.jpg"
+                                                            <img src="https://helostatus.com/wp-content/uploads/2021/09/HD-WhatsApp-profile.jpg" style="    max-width: 20px;"
                                                                 width="20" height="20" />
                                                                 <span style="max-width:160px;font-size: 14px;line-height: 22px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;word-break: break-all;">  {{ $datas->name}}</span>
 
