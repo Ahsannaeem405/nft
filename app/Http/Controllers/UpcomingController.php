d4371f976d05b8ee604a8895596bc241dc6f9b8e
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-26 17:16:23 +0500

    ch

--- app/Http/Controllers/UpcomingController.php
+++ app/Http/Controllers/UpcomingController.php
@@ -36,7 +36,7 @@ class UpcomingController extends Controller
 
 
 
-     $data=Http::get('https://api.opensea.io/api/v1/bundles?limit=20&offset=0');
+     $data=Http::get('https://api.opensea.io/api/v1/bundles?limit=5&offset=0');
         $data=json_decode($data->body());
 
 
