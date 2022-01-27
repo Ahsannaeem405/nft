3368edee8980d8ab140fb9f59f7cad600bb0fefd
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-27 11:21:17 +0500

    ch

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -61,14 +61,13 @@
                                                         </div>
                                                     </li>
                                                     @foreach ($data->bundles as $datas )
-                                                    @dd($datas->asset_contract)
                                                     <li
                                                         class="list-group-item d-flex justify-content-between align-items-center border-top">
                                                         <div class="d-flex justify-content-center align-items-center">
                                                             <span class="mr-2 font-weight-bold font-italic">01</span>
                                                             <img src=" "  style="    max-width: 20px;"
                                                                 width="20" height="20" />
-                                                                <span style="max-width:160px;font-size: 14px;line-height: 22px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;word-break: break-all;">  {{ $datas->name}}</span>
+                                                                <span style="max-width:160px;font-size: 14px;line-height: 22px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;word-break: break-all;">  {{ $datas->asset_contract->name}}</span>
 
                                                         </div>
                                                         <div class="value-size">
