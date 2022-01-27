077fd7a4dd531aec9da3d27ea1efd362d839862b
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-27 11:09:50 +0500

    ch

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -61,18 +61,17 @@
                                                         </div>
                                                     </li>
                                                     @foreach ($data->bundles as $datas )
-                                                    @dd($datas->assets[0])
                                                     <li
                                                         class="list-group-item d-flex justify-content-between align-items-center border-top">
                                                         <div class="d-flex justify-content-center align-items-center">
                                                             <span class="mr-2 font-weight-bold font-italic">01</span>
-                                                            <img src="https://helostatus.com/wp-content/uploads/2021/09/HD-WhatsApp-profile.jpg" style="    max-width: 20px;"
+                                                            <img src="{{$datas->assets[0]->image_preview_url}}" style="    max-width: 20px;"
                                                                 width="20" height="20" />
                                                                 <span style="max-width:160px;font-size: 14px;line-height: 22px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;word-break: break-all;">  {{ $datas->name}}</span>
 
                                                         </div>
                                                         <div class="value-size">
-                                                            $12,50,000.00
+                                                            ${{$datas}}
                                                             <div class="text-success text-right">+28.30%</div>
                                                         </div>
                                                     </li>
