883baad4b8e24a5e65e7100b3549313a7585c6c1
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-31 14:44:38 +0500

    ch

--- app/Http/Controllers/UpcomingController.php
+++ app/Http/Controllers/UpcomingController.php
@@ -224,9 +224,18 @@ return view('admin.edit');
 
         $data=Http::get('https://api.opensea.io/api/v1/collection/'.$slug.'');
         $data=json_decode($data->body());
+
         // dd($data->collection);
         $data1 = $data->collection;
-        return view('user.detail', compact('data1'));
+
+
+        $data12=Http::get('https://api.opensea.io/api/v1/collections?asset_owner=0x495f947276749Ce646f68AC8c248420045cb7b5e&offset=0&limit=12');
+        $data12=json_decode($data12->body());
+
+
+
+
+        return view('user.detail', compact('data1', 'data12'));
 
 
 
--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -417,7 +417,54 @@
         </div>
         <div class="row">
             <div class="col-12" style="    margin-top: 20px;">
-                <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                {{-- <div id="chartContainer" style="height: 370px; width: 100%;"></div> --}}
+                <div class="explore-nft">
+                    <div class="explore-nft-header">
+                        <h2>Explore NFTs</h2>
+                        <div>
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                    <div class="explore-nftbody">
+                        <div class="row">
+                            @foreach ( $data12 as $datass )
+
+                            <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 p-0">
+                                <a href="{{url('/overview',[$datass->slug])}}" style="text-decoration: none;">
+
+                                    <div class="explore-card mt-3 mx-3">
+                                        <div class="explore-card-img">
+                                            <img
+                                           style="    max-height: 190px;min-width: 240px;max-width: 240px;min-height: 190px;" src=" @if (isset($datass->large_image_url)){{$datass->large_image_url}} @else https://lh3.googleusercontent.com/6ryiJWWrm27TkbXYEhfxfhrA5H6eqUQXih8UQSeHx5pwVFdS3jJTBwNXi_9N3EP2pO-czkyKGqV9Pnwvjn8qIAWeWSkkFEpMJlfH=s120  @endif " />
+                                            <i class="fas fa-ellipsis-h"></i>
+                                        </div>
+                                        <div class="explore-card-body m-3">
+                                            <h5 style="line-height: 20px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;word-break: break-all;">{{$datass->name}}</h5>
+                                            <div class="d-flex align-items-center justify-content-between mt-4">
+                                                <div class="explore-card-bodyimg">
+                                                    <img
+                                                        src=" @if(isset($datass->large_image_url)) {{ $datass->large_image_url}} @else https://lh3.googleusercontent.com/6ryiJWWrm27TkbXYEhfxfhrA5H6eqUQXih8UQSeHx5pwVFdS3jJTBwNXi_9N3EP2pO-czkyKGqV9Pnwvjn8qIAWeWSkkFEpMJlfH=s120  @endif " />
+                                                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        class="project-card_icon__3yC3z mx-2">
+                                                        <path
+                                                            d="M3.5 11.448L0 6.417 3.5 8.53 7 6.417l-3.5 5.03zm0-3.5L0 5.76 3.5 0 7 5.76 3.5 7.948z"
+                                                            fill="#8A8A8A"></path>
+                                                    </svg>
+                                                </div>
+                                                <span>{{$datass->stats->total_supply}}ETH</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            @endforeach
+
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
