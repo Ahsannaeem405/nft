0e161c2afd133a98d14ff2688907c085935e8923
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-31 16:29:37 +0500

    sd

--- app/Http/Controllers/UpcomingController.php
+++ app/Http/Controllers/UpcomingController.php
@@ -233,10 +233,12 @@ return view('admin.edit');
         $data12=json_decode($data12->body());
 
         $add2=Http::get('https://api.opensea.io/api/v1/asset/'.$add.'/1');
+
+        dd($add2, 2);
         $add2=json_decode($add2->body());
 
 
-        dd($add2, 2);
+
 
 
         return view('user.detail', compact('data1', 'data12'));
