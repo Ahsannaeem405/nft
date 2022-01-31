ff46b440bbcc2da41e131876f2c7b217042cf557
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-31 15:03:35 +0500

    ch

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -430,7 +430,8 @@
                             <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 p-0">
                                 <a href="{{url('/overview',[$datass->slug])}}" style="text-decoration: none;">
 
-                                    <div class=".explore-card mt-3 mx-3">
+                                    <div class=" mt-3 mx-3" style="    border-radius: 15px;
+                                    border: 1px solid rgba(0, 0, 0, 0.1);">
                                         <div class="explore-card-img" style="    border-radius: 15px;border: 1px solid rgba(0, 0, 0, 0.1);">
                                             <img
                                            style="    max-height: 190px;min-width: 250px;max-width: 250px;min-height: 190px;" src=" @if (isset($datass->large_image_url)){{$datass->large_image_url}} @else https://lh3.googleusercontent.com/6ryiJWWrm27TkbXYEhfxfhrA5H6eqUQXih8UQSeHx5pwVFdS3jJTBwNXi_9N3EP2pO-czkyKGqV9Pnwvjn8qIAWeWSkkFEpMJlfH=s120  @endif " />
