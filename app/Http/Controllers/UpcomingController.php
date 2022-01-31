5acd2d8cad6177d51c130228acf5ca0cb919ae35
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-31 16:24:06 +0500

    ds

--- app/Http/Controllers/UpcomingController.php
+++ app/Http/Controllers/UpcomingController.php
@@ -231,12 +231,12 @@ return view('admin.edit');
 
         $data12=Http::get('https://api.opensea.io/api/v1/collections?asset_owner=0x495f947276749Ce646f68AC8c248420045cb7b5e&offset=0&limit=12');
         $data12=json_decode($data12->body());
-        dd($add);
-        $add2=Http::get('https://api.opensea.io/api/v1/asset_contract/'.$add.'');
+
+        $add2=Http::get('https://api.opensea.io/api/v1/asset_contract/'.$add.'/1');
         $add2=json_decode($add2->body());
 
 
-
+        dd($add2);
 
 
         return view('user.detail', compact('data1', 'data12'));
