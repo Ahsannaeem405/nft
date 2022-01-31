7654ea5847d8cca402b053baa2e032cb02b8a53a
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-31 11:42:01 +0500

    ch

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -180,8 +180,8 @@
 
                                                         </p>
 
-                                                        <h5 style="    margin-bottom: 3px;">${{ $datass->stats->one_day_sales}}</h5>
-                                                        <span style="color: #5cc489;    font-weight: 700;">+ ${{ $datass->stats->one_day_sales}}%</span>
+                                                        <h5 style="    margin-bottom: 3px;">${{ $data1->stats->one_day_sales}}</h5>
+                                                        <span style="color: #5cc489;    font-weight: 700;">+ ${{ $data1->stats->one_day_sales}}%</span>
                                                     </div>
                                                     <div class="col-4" style="  ">
                                                         <div
